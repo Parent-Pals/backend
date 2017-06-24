ef9be358eb9ca31cc22094dd33896a6d5cc87cf6
Matthew Gonzer <matthewgonzer@gmail.com>
2017-06-24 12:18:14 -0600

    add routes

--- db/queries.js
+++ db/queries.js
@@ -2,6 +2,33 @@ const knex = require('./knex');
 
 module.exports = {
   getChildList: function(id){
-    return knex('child').where('parent_id', id).select('*')
+    return knex('child').where('parent_id', id).select('*');
+  },
+  createChild: function(child){
+    return knex('child').insert(child).returning('*');
+  },
+  deleteChild: function(id){
+    return knex('child').where('id', id).del();
+  },
+  getChildPage: function(id){
+    return knex('child').where('child.id', id)
+    .join('task', 'task.child_id', 'child.id')
+    .join('reward', 'reward.child_id', 'child.id')
+    .select('child.id as id', 'child.name as name', 'task.name as task_name', 'task.point_value as task_point_value', 'reward.name as reward_name', 'reward.point_value as reward_point_value')
+  },
+  createTask: function(task){
+    return knex('task').insert(reward).returning('*')
+  },
+  createReward: function(reward){
+    return knex('reward').insert(reward).returning('*')
+  },
+  deleteTask: function(id){
+    return knex('task').where('id', id).del();
+  },
+  deleteReward: function(id){
+    return knex('reward').where('id', id).del();
+  },
+  createParent: function(parent){
+    return knex('parent').insert(parent).returning('*')
   }
 }
--- routes/parents.js
+++ routes/parents.js
@@ -4,9 +4,80 @@ const bcrypt = require('bcrypt');
 const queries = require('../db/queries');
 
 /* GET users listing. */
-router.get('/parent/:id', function(req, res, next) {
-  let children = queries.getChildList(req.params.id);
-  res.send('children');
+router.get('/:id', function(req, res, next) {
+  queries.getChildList(req.params.id)
+    .then(children=>{
+      res.json(children);
+    })
 });
+
+router.post('/:id/', function(req, res){
+  queries.createChild(req.body)
+  .then(results=>{
+    res.send(results[0]);
+  })
+})
+
+router.delete('/:id/:id',function(req, res){
+  queries.deleteChild(req.params.id)
+    .then(()=>{
+      res.json({
+        deleted: true
+      })
+    })
+})
+
+router.get('/:id/:id', function(req, res){
+  queries.getChildPage(req.params.id)
+    .then(child=>{
+      res.json(child)
+    })
+})
+
+router.post('/:id/:id/task', function(req, res){
+  queries.createTask(req.body)
+    .then(tasks=>{
+      res.send(tasks[0])
+    })
+})
+
+router.post('/:id/:id/reward', function(req, res){
+  queries.createReward(req.body)
+    .then(rewards=>{
+      res.send(rewards[0])
+    })
+})
+
+router.delete('/:id/:id/task/:id', function(req, res){
+  queries.deleteTask(req.params.id)
+    .then(()=>{
+      res.json({
+        deleted: true
+      })
+    })
+})
+
+router.delete('/:id/:id/reward/:id', function(req, res){
+  queries.deleteReward(req.params.id)
+    .then(()=>{
+      res.json({
+        deleted: true
+      })
+    })
+})
+
+router.put('/:id/:id', function(req, res){
+  queries.updatePoints(req.params.id, req.body)
+    .then(child=>{
+      res.json(child[0])
+    })
+})
+
+router.post('/', function(req, res){
+  queries.createParent(req.body)
+    .then(parent=>{
+      res.send(parent[0])
+    })
+})
 
 module.exports = router;
