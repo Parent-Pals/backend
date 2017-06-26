d9a63f3de18aa81ec43855108ff75209bda1be67
Marlena Baker <baker.marlena@gmail.com>
2017-06-26 09:40:49 -0600

    small changes

--- routes/parents.js
+++ routes/parents.js
@@ -2,11 +2,14 @@ var express = require('express');
 var router = express.Router();
 const bcrypt = require('bcrypt');
 const queries = require('../db/queries');
+console.log('loading route file')
 
 /* GET users listing. */
-router.get('/parent/:id', function(req, res, next) {
-  let children = queries.getChildList(req.params.id);
-  res.send('children');
+router.get('/', (req, res) => {
+  console.log('Made it to the route!')
+  // let children = queries.getChildList(req.params.id);
+  // res.send(children);
+  res.json({message: "working"})
 });
 
 module.exports = router;
