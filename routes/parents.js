437152e7a98ede7c9c0c96da54dd26fe8ba8938b
Marlena Baker <baker.marlena@gmail.com>
2017-06-29 09:15:08 -0600

    small changes

--- routes/parents.js
+++ routes/parents.js
@@ -55,7 +55,10 @@ router.post('/:id/:childID/task', authMiddleware.allowAccess, function(req, res)
     })
 })
 
-router.post('/reward/:id/:childID', authMiddleware.allowAccess, function(req, res){
+router.post('/:id/child/:childID/reward', authMiddleware.allowAccess, function(req, res){
+  console.log('here reward  ')
+  console.log(req.body)
+  console.log(req.params)
   let childID = req.params.childID;
   let reward = {name: req.body.name, point_value: req.body.point_value, child_id: childID};
   queries.createReward(reward)
